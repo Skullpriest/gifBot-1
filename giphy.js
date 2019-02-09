5a12dac11ec0fe875c1054d3313bfd1811a74ad3
SamuelNIttala <nittalasamuel7@gmail.com>
2019-02-09 12:23:58 +0530

    Delete giphy.js

--- giphy.js
+++
@@ -1,7 +0,0 @@
-
-const fetch = require('fetch').fetchUrl;
-
-fetch('http://api.giphy.com/v1/gifs/search?q=funnycat&api_key=9Wl7MFx25spbL0klJEPqt3tgPa3GHUvY',(err,meta,body)=>{
-    const data=JSON.parse(body)
-    console.log(data.data[0].images)
-})
